6ae594af1f399733f3b1cde5d134e951cf16249d
Gaurav4750 <89590945+Gaurav4750@users.noreply.github.com>
2022-01-22 11:43:24 +0530

    Create PrimeNumber.py

---
+++ PrimeNumber.py
@@ -0,0 +1,17 @@
+# Program to validate prime number and find divisible numbers.
+
+flag = False
+number = input("Enter the number: ")
+if int(number)>1:
+    for j in range(2, int(number)):
+        if (int(number) % j) == 0:
+            flag = True           
+if flag:
+    print(str(number)+" is a not a Prime Number.")
+else:
+    print(str(number)+" is a Prime Number.")
+
+for i in range (2, int(number)//2+1):
+    if int(number)%i !=0:
+        continue
+    print("%s is divisible by %d"%(number,i))
