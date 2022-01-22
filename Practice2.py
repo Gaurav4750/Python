b9701c00f2665974f74c475a3114678de41d4b7a
Gaurav4750 <89590945+Gaurav4750@users.noreply.github.com>
2022-01-22 11:46:15 +0530

    Add files via upload

---
+++ Practice2.py
@@ -0,0 +1,16 @@
+# The number divisible
+flag = False
+number = input("Enter the number: ")
+if int(number)>1:
+    for j in range(2, int(number)):
+        if (int(number) % j) == 0:
+            flag = True           
+if flag:
+    print(str(number)+" is a not a Prime Number.")
+else:
+    print(str(number)+" is a Prime Number.")
+
+for i in range (2, int(number)//2+1):
+    if int(number)%i !=0:
+        continue
+    print("%s is divisible by %d"%(number,i))
